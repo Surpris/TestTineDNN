23b2a0775627f3c76fdd1927dd866deb94bcb042
Surpris <take90-it09-easy27@outlook.jp>
2017-06-22 10:50:13 +0900

    モデルを定義する場所としてのヘッダーファイルを追加。

---
+++ TestTinyDNN/Models.h
@@ -0,0 +1,24 @@
+#include "Header.h"
+
+using namespace std;
+using namespace tiny_dnn;
+
+// Basic sequential model.
+network<sequential> create_model_1(const vector<vec_t> X, const vector<vec_t> y) {
+	network<sequential> model;
+	size_t n_neural = 100;
+	model << fc(X[0].size(), n_neural) << activation::tanh()
+		<< fc(n_neural, y[0].size()) << activation::softmax();
+	return model;
+}
+
+// multilayer sequantial model with a dropout layer.
+network<sequential> create_model_2(const vector<vec_t> X, const vector<vec_t> y) {
+	network<sequential> model;
+	size_t n_neural = 100;
+	model << fc(X[0].size(), n_neural) << activation::tanh()
+		<< fc(n_neural, n_neural) << activation::sigmoid()
+		<< dropout(n_neural, 0.3)
+		<< fc(n_neural, y[0].size()) << activation::softmax();
+	return model;
+}
